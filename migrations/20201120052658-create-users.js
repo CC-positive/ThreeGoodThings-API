8071ff0f06a3384f7b687f19d5f366de475c70b6
TomoyaHirano <tomoya.hirano.th@gmail.com>
2020-11-20 14:29:52 +0900

    initial commit

---
+++ migrations/20201120052658-create-users.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: Sequelize.literal('uuid_generate_v4()'),
+        type: Sequelize.UUID,
+      },
+      userName: {
+        type: Sequelize.STRING
+      },
+      token: {
+        type: Sequelize.STRING
+      },
+      googleId: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
