9253bcef3d4c967a5c2455b094e57e0662405e41
yu-kaburagi <longtalleyelash@gmail.com>
2020-11-20 22:01:25 +0900

    fix seed files

--- seeders/20201120060137-test-posts.js
+++ seeders/20201120060137-test-posts.js
@@ -3,48 +3,16 @@ const db = require("../models/index");
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    // const now = new Date();
-    // const ichiroId = "";
-    // const jichiroId = "";
-    // const sabuchiroId = "";
-
-    // db.users
-    // .findOne({
-    //   attributes: ["id"],
-    //   where: {
-    //     userName:"一郎"
-    //   },
-    // })
-    // .then((data) => {
-    //   ichiroId = data.id
-    // });      
-
-    // db.users
-    // .findOne({
-    //   attributes: ["id"],
-    //   where: {
-    //     userName:"二郎"
-    //   },
-    // })
-    // .then((data) => {
-    //   jichiroId = data.id
-    // });      
-
-    // db.users
-    // .findOne({
-    //   attributes: ["id"],
-    //   where: {
-    //     userName:"三郎"
-    //   },
-    // })
-    // .then((data) => {
-    //   sabuchiroId = data.id
-    // });      
-
-    // return queryInterface.bulkInsert('posts', [
-    //   { userId: '4a1f6310-1b15-4c10-8fd4-fd2140e8d03b',  date: now, createdAt: now, updatedAt: now},
-    //   { userId: '4a1f6310-1b15-4c10-8fd4-fd2140e8d03b',  date: now, createdAt: now, updatedAt: now}
-    // ], {});
+    const now = new Date();
+    const result1 = await db.users.findOne({raw: true, attributes: ["id"],where: {userName:"一郎"}});
+    const result2 = await db.users.findOne({raw: true, attributes: ["id"],where: {userName:"二郎"}});
+    const result3 = await db.users.findOne({raw: true, attributes: ["id"],where: {userName:"三郎"}});
+    queryInterface.bulkInsert('posts', [
+      { userId: result1.id,  date: now, createdAt: now, updatedAt: now},
+      { userId: result2.id,  date: now, createdAt: now, updatedAt: now},
+      { userId: result3.id,  date: now, createdAt: now, updatedAt: now}
+    ], {});
+    
   },
 
   down: async (queryInterface, Sequelize) => {
