deefe8749f467cd28905da79423308b4cd3f5aa8
h-tsuboi918 <hiroki.tsuboi.918@gmail.com>
2020-11-25 00:01:15 +0900

    [fix] fix to return 200 if options request

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const createError = require('http-errors');
-const express = require('express');
-let path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const fetch = require('node-fetch');
+const createError = require("http-errors");
+const express = require("express");
+let path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const fetch = require("node-fetch");
 
 // const { validate } = require("uuid"); //unused vars
-const loginRouter = require('./routes/login');
-const postsRouter = require('./routes/posts');
-const rewardsRouter = require('./routes/rewards');
+const loginRouter = require("./routes/login");
+const postsRouter = require("./routes/posts");
+const rewardsRouter = require("./routes/rewards");
 
 const app = express();
 
@@ -16,37 +16,36 @@ const app = express();
  * Enable CORS request
  */
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://threetter.tk:3000');
+  res.header("Access-Control-Allow-Origin", "http://threetter.tk:3000");
   res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, x-auth-token, x-googleid',
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-auth-token, x-googleid"
   );
-  next();
-
-  app.options('*', (req, res) => {
+  app.options("*", (req, res) => {
     // allowed XHR methods
     res.header(
-      'Access-Control-Allow-Methods',
-      'GET, PATCH, PUT, POST, DELETE, OPTIONS',
+      "Access-Control-Allow-Methods",
+      "GET, PATCH, PUT, POST, DELETE, OPTIONS"
     );
-    res.send();
+    res.send(200);
   });
+  next();
 });
 
 // Authentication before api execution using Google OAuth2 API
 app.use(async (req, res, next) => {
-  if (process.env.NODE_ENV !== 'production') {
+  if (process.env.NODE_ENV !== "production") {
     next();
     return;
   }
   let idToken;
-  if (req.headers['x-auth-token']) {
-    idToken = req.headers['x-auth-token'];
+  if (req.headers["x-auth-token"]) {
+    idToken = req.headers["x-auth-token"];
   } else {
     next(createError(403));
     return;
   }
-  const validatePath = 'https://oauth2.googleapis.com/tokeninfo?id_token=';
+  const validatePath = "https://oauth2.googleapis.com/tokeninfo?id_token=";
   path = validatePath + idToken;
   const authRes = await fetch(path);
   if (authRes.status === 200) {
@@ -59,18 +58,18 @@ app.use(async (req, res, next) => {
 });
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/v1/threetter/login', loginRouter);
-app.use('/v1/threetter/posts', postsRouter);
-app.use('/v1/threetter/rewards', rewardsRouter);
+app.use("/v1/threetter/login", loginRouter);
+app.use("/v1/threetter/posts", postsRouter);
+app.use("/v1/threetter/rewards", rewardsRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -81,11 +80,11 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
   next();
 });
 
