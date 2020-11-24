91f9bb95a7a7c54b6eef2354a48ed29e7c355265
TomoyaHirano <tomoya.hirano.th@gmail.com>
2020-11-24 16:14:30 +0900

    add Backend: ユーザ登録API（ログインAPIの作成） #37

--- seeders/20201120053617-test-users.js
+++ seeders/20201120053617-test-users.js
@@ -1,18 +1,35 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const now = new Date();
-    return queryInterface.bulkInsert('users', [
-      {
-        userName: '一郎', token: 'hogetoken', googleId: 'hogegoogleid', createdAt: now, updatedAt: now,
-      },
-      {
-        userName: '二郎', token: 'hogetoken', googleId: 'hogegoogleid', createdAt: now, updatedAt: now,
-      },
-      {
-        userName: '三郎', token: 'hogetoken', googleId: 'hogegoogleid', createdAt: now, updatedAt: now,
-      },
-    ], {});
+    return queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          userName: "一郎",
+          picture: "hogetoken",
+          googleId: "hogegoogleid",
+          createdAt: now,
+          updatedAt: now,
+        },
+        {
+          userName: "二郎",
+          picture: "hogetoken",
+          googleId: "hogegoogleid",
+          createdAt: now,
+          updatedAt: now,
+        },
+        {
+          userName: "三郎",
+          picture: "hogetoken",
+          googleId: "hogegoogleid",
+          createdAt: now,
+          updatedAt: now,
+        },
+      ],
+      {}
+    );
   },
 
-  down: async (queryInterface, Sequelize) => queryInterface.bulkDelete('users', null, {}),
+  down: async (queryInterface, Sequelize) =>
+    queryInterface.bulkDelete("users", null, {}),
 };
