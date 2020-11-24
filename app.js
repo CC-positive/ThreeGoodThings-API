d33117713dc3e0c9c9a28908a31dfb41ba98be21
h-tsuboi918 <hiroki.tsuboi.918@gmail.com>
2020-11-24 17:56:07 +0900

    [fix] fix Access-Control-Allow-Origin

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
  * Enable CORS request
  */
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://18.181.45.23:3000');
+  res.header('Access-Control-Allow-Origin', 'http://threetter.tk:3000');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept',
